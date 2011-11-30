e48c42f9b7e589465b6936cc89e356f00d38c1b3
mnicky <xmnicky@gmail.com>
2011-11-30 15:46:48 +0100

    deploy_pkg() and install_pkg() added

--- dist/init.lua
+++ dist/init.lua
@@ -6,10 +6,11 @@ local cfg = require "dist.config"
 local dep = require "dist.depends"
 local git = require "dist.git"
 local sys = require "dist.sys"
+local mf = require "dist.manifest"
 
 -- Install package_names to deploy_dir
 function install(package_names, deploy_dir)
-    if not package_names then return end
+    if not package_names then return true end
 
     deploy_dir = deploy_dir or cfg.root_dir
     if type(package_names) == "string" then package_names = {package_names} end
@@ -21,19 +22,100 @@ function install(package_names, deploy_dir)
     local dependencies, err = dep.get_depends(package_names, deploy_dir)
     if err then return nil, err end
 
+    -- TODO install packages after fetching all of them
+
     for _, pkg in pairs(dependencies) do
 
         -- TODO get tmp dir from configuration?
         -- fetch the package from git repository
-        fetch_pkg(pkg, deploy_dir .. "/tmp")
+        local ok, dir_or_err = fetch_pkg(pkg, deploy_dir .. "/tmp")
+
+        if not ok then
+            return nil, dir_or_err
+        else
+            ok, err = install_pkg(dir_or_err)
+            if not ok then return nil, err end
+        end
+    end
+end
 
 
+-- Install package from 'pkg_dir' to 'deploy_dir'
+function install_pkg(pkg_dir, deploy_dir)
 
+    deploy_dir = deploy_dir or cfg.root_dir
+
+    assert(type(pkg_dir) == "string", "dist.make_pkg: Argument 'pkg_dir' is not a string.")
+    assert(type(deploy_dir) == "string", "dist.make_pkg: Argument 'deploy_dir' is not a string.")
+
+    -- check for dist.info
+    local info, err = mf.load_distinfo(pkg_dir .. "/dist.info")
+    if not info then return nil, "Error installing '" .. pkg.name .. "-" .. pkg.version .. "': package in '" .. pkg_dir .. "' doesn't contain valid 'dist.info' file." end
+
+    -- check if the package is source
+    if sys.exists(pkg_dir .. "/CMakeLists.txt") then
+        info.arch = info.arch or "Universal"
+        info.type = info.type or "source"
     end
 
+    -- check package's architecture
+    if info.arch ~= "Universal" and info.arch ~= cfg.arch then
+        return nil, "Error installing '" .. info.name .. "-" .. info.version .. "': architecture '" .. info.arch .. "' is not suitable for this machine."
+    end
 
+    -- check package's type
+    if info.type ~= "all" and info.type ~= "source" and info.type ~= cfg.type then
+        return nil, "Error installing '" .. info.name .. "-" .. info.version .. "': architecture type '" .. info.type .. "' is not suitable for this machine."
+    end
 
+    local ok, err
+
+    -- if package is of source type, just deploy it
+    if info.type ~= "source" then
+        ok, err = deploy_pkg(pkg_dir, deploy_dir)
+    -- else build the package
+    else
+        -- TODO implement build_pkg():
+        -- ok, err = build_pkg(pkg_dir, deploy_dir)
+        ok = true
+    end
+
+    -- delete directory of fetched package
+    if not cfg.debug then sys.delete(pkg_dir) end
+
+    return ok, err
 end
+
+-- Deploy package from 'pkg_dir' to 'deploy_dir' by copying
+function deploy_pkg(pkg_dir, deploy_dir)
+
+    deploy_dir = deploy_dir or cfg.root_dir
+
+    assert(type(pkg_dir) == "string", "dist.deploy_pkg: Argument 'pkg_dir' is not a string.")
+    assert(type(deploy_dir) == "string", "dist.deploy_pkg: Argument 'deploy_dir' is not a string.")
+
+    -- check for dist.info
+    local info, err = mf.load_distinfo(pkg_dir .. "/dist.info")
+    if not info then return nil, "Error deploying package from '" .. pkg_dir .. "': it doesn't contain valid 'dist.info' file." end
+
+    -- delete the 'dist.info' file
+    sys.delete(pkg_dir .. "/dist.info")
+
+    -- copy all files to the deploy_dir
+    local ok, err = sys.copy(pkg_dir .. "/.", deploy_dir)
+    if not ok then return nil, "Error deploying package '" .. info.name .. "-" .. info.version .. "': " .. err end
+
+    -- save modified 'dist.info' file
+    info.files = sys.get_file_list(pkg_dir)
+    local pkg_distinfo_dir = deploy_dir .. "/" .. cfg.distinfos_dir .. "/" .. info.name .. "-" .. info.version
+    sys.make_dir(pkg_distinfo_dir)
+
+    ok, err = mf.save_distinfo(info, pkg_distinfo_dir .. "/dist.info")
+    if not ok then return nil, err end
+
+    return true, "Package '" .. info.name .. "-" .. info.version .. "' successfully deployed to '" .. deploy_dir .. "'."
+end
+
 
 -- Fetch package (table 'pkg') to download_dir
 -- Return if the operation was successful and a path to the directory on success or an error message on error.
