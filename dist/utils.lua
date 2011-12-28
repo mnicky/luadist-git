50c795d7a5cb8b049444a6561b23640d3b9540c4
mnicky <xmnicky@gmail.com>
2011-12-28 14:55:21 +0100

    dist.utils module created

---
+++ dist/utils.lua
@@ -0,0 +1,23 @@
+-- System functions
+
+module ("dist.utils", package.seeall)
+
+-- Returns a deep copy of 'table' with reference to the same metadata table.
+-- Source: http://lua-users.org/wiki/CopyTable
+function deepcopy(object)
+    local lookup_table = {}
+    local function _copy(object)
+        if type(object) ~= "table" then
+            return object
+        elseif lookup_table[object] then
+            return lookup_table[object]
+        end
+        local new_table = {}
+        lookup_table[object] = new_table
+        for index, value in pairs(object) do
+            new_table[_copy(index)] = _copy(value)
+        end
+        return setmetatable(new_table, getmetatable(object))
+    end
+    return _copy(object)
+end
